366b07f9a7505e58a7a7756c2de385e2730fa278
yangyakun <yangyakun@zhiyihealth.com.cn>
2019-03-04 21:22:58 +0800

    动态代理对象的修改

--- src/main/java/com/yangyakun/javaTool/pattern/proxy/dnamicproxy/Client.java
+++ src/main/java/com/yangyakun/javaTool/pattern/proxy/dnamicproxy/Client.java
@@ -2,7 +2,11 @@ package com.yangyakun.javaTool.pattern.proxy.dnamicproxy;
 
 import com.yangyakun.javaTool.pattern.proxy.demo05.ILawsuit;
 import com.yangyakun.javaTool.pattern.proxy.demo05.XiaoMing;
+import sun.misc.ProxyGenerator;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Proxy;
 
 public class Client {
@@ -19,5 +23,20 @@ public class Client {
         lawyer.burden();
         lawyer.defend();
         lawyer.finish();
+
+        createFiel();
+    }
+
+    public static void createFiel(){
+        byte[] Proxy0s = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{XiaoMing.class});
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("$Proxy0.class");
+            fileOutputStream.write(Proxy0s);
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
