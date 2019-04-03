9535363446b12dccc8c6b617f3fa367a2802576a
yangyakun <361176335@qq.com>
2019-04-03 17:33:08 +0800

    poi 签名

--- src/main/java/com/yangyakun/javaTool/Test.java
+++ src/main/java/com/yangyakun/javaTool/Test.java
@@ -1,12 +1,26 @@
 package com.yangyakun.javaTool;
 
 import com.yangyakun.javaTool.date.Formatter;
+import com.yangyakun.javaTool.random.RandomUntil;
 
 import java.util.Calendar;
 import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
+
+
+        for (int i = 1; i < 31; i++) {
+            int num = i;
+            System.out.print("共"+num+"个患者");
+            int lastPage = num % 6;
+            System.out.print("最后一页"+ lastPage);
+            if(lastPage==0) {
+                System.out.println("共几页" + num / 6);
+            }else {
+                System.out.println("共几页" + (num / 6+1));
+            }
+        }
 
         try {
             System.out.println(Double.valueOf("1.0").intValue());
